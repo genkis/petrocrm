7ce7d8c21dbffb612b6f14ea28b696764b72843e
Piotr Kiszka <piotrk@adventresources.com>
2021-01-17 12:52:59 +0100

    [PCRM-3] Create a functionality for getting JWT token

---
+++ backend/app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "petrocrm-17012021-secret"
+};
--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,42 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const db = require("./app/models");
+const Role = db.role;
+
+//force: true - drop tables and re-sync database
+//In production use sync without parameters
+db.sequelize.sync({force: true}).then(() => {
+    console.log('Drop and Resync Db');
+    initial();
+});
+
+function initial() {
+    Role.create({
+                    id: 1,
+                    name: "user"
+                });
+
+    Role.create({
+                    id: 2,
+                    name: "moderator"
+                });
+
+    Role.create({
+                    id: 3,
+                    name: "admin"
+                });
+}
+
 // simple route
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to PetroCRM application." });
 });
+
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
